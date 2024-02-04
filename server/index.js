fc7722ff4bfc76571130ecec8f59c4f7fecf6a4a
iamrachitgaur <rachitgaur3@gmail.com>
2024-02-04 19:20:44 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const database = require('./server/db/database')
+const database = require('./db/database')
 const app = express();
 const port = process.env.PORT || 4200;
-const userRouter = require('./server/routes/userRouter');
-const shopRouter = require('./server/routes/shopRouter');
-const timeSlotRouter = require('./server/routes/timeSlotRouter');
+const userRouter = require('./routes/userRouter');
+const shopRouter = require('./routes/shopRouter');
+const timeSlotRouter = require('./routes/timeSlotRouter');
 
 app.use(express.static('./dist/shears/'))
 app.use(express.json())
@@ -12,7 +12,7 @@ app.use('/userApi',userRouter)
 app.use('/shopApi',shopRouter)
 app.use('/timeSlotApi',timeSlotRouter)
 app.get('/*', (req, res) =>
-    res.sendFile('index.html',{root:`${__dirname}/dist/shears/index.html`})
+    res.sendFile('index.html',{root:`${__dirname}/../dist/shears/index.html`})
 );
 
 app.listen(port,()=>{console.log(`app listen on port : ${port}`)});
